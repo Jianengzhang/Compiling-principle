56958d0d16de51d4f70fc2e87f4cd6788201cab7
DESKTOP-4QNAVNC\Aron <jianengz2058@foxmail.com>
2017-12-19 21:16:02 +0800

    文件路径修改

--- src/java/MainApplication.java
+++ src/java/MainApplication.java
@@ -1,13 +1,15 @@
 import grammatical.GrammaticalAnalysis;
 import intermediateCode.IntermediateCodeGeneration;
 import lexical.LexicalAnalysis;
+import sun.applet.Main;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainApplication {
     public static void main(String args[]){
-        String lexicalPath = "D:/input.txt";
+        String lexicalPath = System.getProperty("user.dir")+"/src/resourse/input.txt";
         LexicalAnalysis lexicalAnalysis = new LexicalAnalysis();
         GrammaticalAnalysis grammaticalAnalysis =new GrammaticalAnalysis();
         List<String> strings = new ArrayList<>();
